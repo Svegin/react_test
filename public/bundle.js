a71b39388e6c13a53baf2b837836c20dc9408e99
Svegin <svegin@Svegins-Mac-mini.local>
2017-03-03 23:51:29 -0600

    correct files

--- public/bundle.js
+++ public/bundle.js
@@ -1,4 +1,28 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-console.log('hello react & redux');
+const cards =(state, action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date
+      });
+      return state.concat([newCard]);
+
+    default:
+    return state || [];
+  }
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+  cards
+}));
+
+const App = (props) => {
+  return (`div className='app'
+      <h1>Hello React</h1>
+    /div`);
+};
+
+ReactDOM.render(`App /`, document.getElementById('root'));
 
 },{}]},{},[1]);
--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
-console.log('hello react & redux');
+const cards =(state, action) => {
+  switch (action.type) {
+    case 'ADD_CARD':
+      let newCard = Object.assign({}, action.data, {
+        score: 1,
+        id: +new Date
+      });
+      return state.concat([newCard]);
+
+    default:
+    return state || [];
+  }
+};
+
+const store = Redux.createStore(Redux.combineReducers({
+  cards
+}));
+
+const App = (props) => {
+  return (`div className='app'
+      <h1>Hello React</h1>
+    /div`);
+};
+
+ReactDOM.render(`App /`, document.getElementById('root'));
